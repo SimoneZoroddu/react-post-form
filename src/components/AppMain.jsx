94020c3d623764d06113df82d4359ea241b17a2f
SimoneZoroddu <lenviur@gmail.com>
2026-03-18 18:51:42 +0100

    adjust exercise with catch error

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,58 +1,45 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import axios from 'axios'
+const oggetto = {
+    author: '',
+    title: '',
+    body: '',
+    public: false
+}
 
 export default function AppMain() {
-/* 
-    const [author, setAuthor] = useState('')
-    const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
-    const [overt, setOvert] = useState(false) */
-   
-   
-   
-    
-        const [object, setObject] = useState({
-            author: '',
-            title: '',
-            body: '',
-            public: false
-        })
-    
-        function handleInput(e) {
-        setObject({...object, [e.target.name] : e.target.type == 'checkbox' ? e.target.checked : e.target.value})
-        }
 
+    const [object, setObject] = useState(oggetto)
 
-
-    
-
+    function handleInput(e) {
+        setObject({ ...object, [e.target.name]:  e.target.checked || e.target.value })
+    }
 
 
     function postObject(e) {
         e.preventDefault()
-        const logaxios = axios({
-            method: 'post',
-            url: 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts',
-            data: {object}
-        });
-        console.log(logaxios)
+        axios.post('https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts', object)
+            .then((res) => {
+                setObject(oggetto)
+                console.log(res.data)
+                alert('Dati inviati al server')
+            }
+            ).catch(error => {
+                alert('Something go wrong', error)
+            })
     }
-
-
-
 
 
     return (
         <div className="container text-center">
-            <h1 className="text-success">Aggiungi all Api i tuoi dati d'autore</h1>
-            <form action="" onSubmit={postObject} >
-                <input className="form-control mb-3" type="text" name="author" value={object.author} onChange={handleInput} />
-                <input className="form-control mb-3" type="text" name="title" value={object.title} onChange={handleInput} />
-                <textarea className="form-control mb-3" type="text" name="body" value={object.body} onChange={handleInput} />
-                <input type="checkbox" name="public" className='form-check-input' value={object.public} onChange={handleInput} /> Autore Publico? <hr />
+            <h1 className="text-success">Aggiungi all Api i tuoi dati d'autore e film</h1>
+            <form action="" onSubmit={postObject}>
+                <input placeholder='Nome Autore' className="form-control mb-3" type="text" name="author" value={object.author} onChange={handleInput} />
+                <input placeholder='Titolo film' className="form-control mb-3" type="text" name="title" value={object.title} onChange={handleInput} />
+                <textarea placeholder='Descrizione' className="form-control mb-3" type="text" name="body" value={object.body} onChange={handleInput} />
+                <input type="checkbox" name="public" className='form-check-input form-switch' value={object.public} onChange={handleInput} /> Autore Publico? <hr />
                 <button className='btn btn-primary'>Aggiungi l'Autore</button>
             </form>
-
         </div>
     )
 
